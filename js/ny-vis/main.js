5f19fabfc38084cb4537673a78e082803efe9034
Katherine McPhie <katherinemcphie@Katherines-MacBook-Air.local>
2023-11-25 21:08:02 -0500

    preliminary ny vis finished

--- js/ny-vis/main.js
+++ js/ny-vis/main.js
@@ -1,39 +1,26 @@
-// Variable for the visualization instance
+/* * * * * * * * * * * * * *
+*           MAIN           *
+* * * * * * * * * * * * * */
+
+// General New York references:
+// - mean
+// - come back
+// - holy ground
+// - welcome to New York
+// - false God
+// - daylight
+// - hoax
+
 let myNYMap;
 
-
-// Hubway JSON feed
-let url = 'https://gbfs.bluebikes.com/gbfs/en/station_information.json';
-
-d3.json(url).then(jsonData =>{
-    // console.log(jsonData);
-});
-
-fetch(url, function (d) {
+fetch('https://gbfs.bluebikes.com/gbfs/en/station_information.json', function (d) {
     console.log(d)
 })
     .then(response => response.json())
     .then(data => {
-        gettingStarted(data)
+        initVis(data)
     });
 
-
-// function that gets called once data has been fetched.
-// We're handing over the fetched data to this function.
-// From the data, we're creating the final data structure we need and create a new instance of the NYMap
-function gettingStarted(data) {
-
-    // log data
-    // console.log(data)
-
-    // Extract data from JSON response
-
-    // create empty data structure
-    let displayData = [];
-
-    // Populate empty data structure
-
-
-    // Instantiate visualization object
-    myNYMap = new NYMap("ny-vis", displayData, [42.360082, -71.058880]); // [40.72332345541449, -73.99]
+function initVis(data) {
+    myNYMap = new NYMap("ny-vis", data, [40.758896, -73.985130]);
 }
--- js/ny-vis/ny.js
+++ js/ny-vis/ny.js
@@ -3,6 +3,10 @@
 * * * * * * * * * * * * * */
 
 class NYMap {
+
+    /*
+     *  Constructor method
+     */
     constructor(parentElement, displayData, mapCenter) {
         this.parentElement = parentElement;
         this.displayData = displayData;
@@ -25,54 +29,12 @@ class NYMap {
         vis.map = L.map(vis.parentElement).setView(vis.mapCenter, 13);
 
         // Load and display a tile layer on the map
-        L.tileLayer("http://{s}.tiles.mapbox.com/v3/mapbox.mapbox-light/{z}/{x}/{y}.png", {
-            minZoom: 10, maxZoom: 13
+        L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+            attribution: 'OpenStreetMap'
         }).addTo(vis.map);
 
-
-
-        // GeoJSON layer
-
-        // let promises = [
-        //     d3.json("data/MBTA-Lines.json")
-        // ];
-        //
-        // Promise.all(promises)
-        //     .then(function (data) {
-        //         console.log(data);
-        //         data.forEach(geoJSONData => {
-        //             // Create a GeoJSON layer and add it to the map
-        //             L.geoJSON(geoJSONData, {
-        //                 style: function (feature) {
-        //                     // Access properties of each line and style accordingly
-        //                     return {
-        //                         color: (function () {
-        //                             switch (feature.properties.LINE) {
-        //                                 case 'RED':
-        //                                     return 'red';
-        //                                 case 'GREEN':
-        //                                     return 'green';
-        //                                 case 'ORANGE':
-        //                                     return 'orange';
-        //                                 case 'BLUE':
-        //                                     return 'blue';
-        //                                 case 'SILVER':
-        //                                     return 'gray';
-        //                                 default:
-        //                                     return 'black';
-        //                             }
-        //                         })(),
-        //                         weight: 15,
-        //                         opacity: 0.6
-        //                     };
-        //                 }
-        //             }).addTo(vis.map);
-        //         });
-        //     })
-        //     .catch(function (err) {
-        //         console.log(err);
-        //     });
-
+        // Create a LayerGroup to hold the station markers
+        vis.stationLayer = L.layerGroup().addTo(vis.map);
 
         vis.wrangleData();
     }
@@ -84,12 +46,62 @@ class NYMap {
     wrangleData () {
         let vis = this;
 
+        // No data wrangling/filtering needed
+
+        // Update the visualization
         vis.updateVis();
     }
 
     updateVis() {
         let vis = this;
 
-        //
+        // Clear the existing markers from the LayerGroup
+        vis.stationLayer.clearLayers();
+
+        // Create an array of locations referenced
+        // note: coordinates from latlong.net
+        let locations = [];
+        locations.push(L.marker([40.747991, -74.004765])
+            .bindPopup("The Highline (Cardigan)"));
+        locations.push(L.marker([40.5755, -73.9700])
+            .bindPopup("Coney Island (Evermore)"));
+        locations.push(L.marker([40.7328, -74.0020])
+            .bindPopup("Cornelia Street (Cornelia Street)"));
+        locations.push(L.circle([40.7265, -73.9840], 500, {
+                color: 'blue',
+                fillColor: 'blue',
+                fillOpacity: 0.5
+            })
+            .bindPopup("Dive bar on the east side (Delicate)"));
+        locations.push(L.marker([40.7486, -73.9857])
+            .bindPopup("Madison Square (The Lucky One)"));
+        locations.push(L.circle([40.7505, -73.9934], 500, {
+                color: 'blue',
+                fillColor: 'blue',
+                fillOpacity: 0.5
+            })
+            .bindPopup("Garden (Cruel Summer)"));
+        locations.push(L.circle([40.7128, -74.0060], 500, {
+                color: 'blue',
+                fillColor: 'blue',
+                fillOpacity: 0.5
+            })
+            .bindPopup("Office downtown (You Are In Love)"));
+        locations.push(L.polygon([
+                [42.837020, -75.397810],
+                [42.881160, -73.731130],
+                [44.986840, -73.448110],
+                [45.0180, -74.7286]],
+            {
+                color: "blue",
+                fillOpacity: 0.5,
+                weight: 3
+                })
+            .bindPopup("Upstate (All Too Well)"));
+
+        // Add each location referenced to map
+        locations.forEach(object => {
+            object.addTo(vis.map);
+        })
     }
 }
